91cf0725c586dcf10deb86a1777d2e210b09b8c0
Gil Raphaelli <gil@elastic.co>
2022-12-08 10:10:08 -0500

    omitempty everything

--- metricize.go
+++ metricize.go
@@ -11,15 +11,15 @@ type DurationHistogram struct {
 
 type Transaction struct {
 	Name              string `json:"name"`
-	Root              bool   `json:"root"`
-	Result            string `json:"result"`
+	Root              bool   `json:"root,omitempty"`
+	Result            string `json:"result,omitempty"`
 	Type              string `json:"type"`
 	DurationHistogram `json:"duration.histogram"`
 }
 
 type MetricDoc struct {
 	Timestamp time.Time `json:"@timestamp"`
-	DocCount  int64     `json:"_doc_count"`
+	DocCount  int64     `json:"_doc_count,omitempty"`
 	//Labels    struct {
 	//	CountryCode string `json:"country_code"`
 	//	InEu        string `json:"in_eu"`
@@ -28,57 +28,57 @@ type MetricDoc struct {
 	//	Lang        string `json:"lang"`
 	//} `json:"labels"`
 	Agent struct {
-		Name string `json:"name"`
-	} `json:"agent"`
+		Name string `json:"name,omitempty"`
+	} `json:"agent,omitempty"`
 	Cloud struct {
-		Provider         string `json:"provider"`
-		Region           string `json:"region"`
-		AvailabilityZone string `json:"availability_zone"`
+		Provider         string `json:"provider,omitempty"`
+		Region           string `json:"region,omitempty"`
+		AvailabilityZone string `json:"availability_zone,omitempty"`
 		Account          struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"account"`
+			ID   string `json:"id,omitempty"`
+			Name string `json:"name,omitempty"`
+		} `json:"account,omitempty"`
 		Machine struct {
-			Type string `json:"type"`
-		} `json:"machine"`
+			Type string `json:"type,omitempty"`
+		} `json:"machine,omitempty"`
 		Project struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"project"`
-	}
+			ID   string `json:"id,omitempty"`
+			Name string `json:"name,omitempty"`
+		} `json:"project,omitempty"`
+	} `json:"cloud,omitempty"`
 	Container struct {
-		ID string `json:"id"`
-	} `json:"container"`
+		ID string `json:"id,omitempty"`
+	} `json:"container,omitempty"`
 	Host struct {
-		Name     string `json:"name"`
-		Hostname string `json:"hostname"`
+		Name     string `json:"name,omitempty"`
+		Hostname string `json:"hostname,omitempty"`
 		OS       struct {
-			Platform string `json:"platform"`
-		} `json:"os"`
-	}
+			Platform string `json:"platform,omitempty"`
+		} `json:"os,omitempty"`
+	} `json:"host,omitempty"`
 	Event struct {
-		Outcome string `json:"outcome"`
-	}
+		Outcome string `json:"outcome,omitempty"`
+	} `json:"event,omitempty"`
 	Kubernetes struct {
 		Pod struct {
-			Name string `json:"name"`
-		} `json:"pod,omitempty"`
-	}
+			Name string `json:"name,omitempty"`
+		} `json:"pod,omitempty,omitempty"`
+	} `json:"kubernetes,omitempty"`
 	Service struct {
-		Environment string `json:"environment"`
-		Name        string `json:"name"`
+		Environment string `json:"environment,omitempty"`
+		Name        string `json:"name,omitempty"`
 		Node        struct {
-			Name string `json:"name"`
-		} `json:"node"`
+			Name string `json:"name,omitempty"`
+		} `json:"node,omitempty"`
 		Language struct {
-			Name    string `json:"name"`
-			Version string `json:"version"`
-		} `json:"language"`
+			Name    string `json:"name,omitempty"`
+			Version string `json:"version,omitempty"`
+		} `json:"language,omitempty"`
 		Runtime struct {
-			Name    string `json:"name"`
-			Version string `json:"version"`
-		} `json:"runtime"`
-		Version string `json:"version"`
-	} `json:"service"`
+			Name    string `json:"name,omitempty"`
+			Version string `json:"version,omitempty"`
+		} `json:"runtime,omitempty"`
+		Version string `json:"version,omitempty"`
+	} `json:"service,omitempty"`
 	Transaction `json:"transaction"`
 }
